3415f0954121f38e6101e7624494f9b0d9f48590
Khizer Tariq <99535489+khizer-kt@users.noreply.github.com>
2023-05-15 20:00:20 +0500

    Added Time Functions

--- naive.py
+++ naive.py
@@ -1,12 +1,25 @@
+import time
+
 def naive_search(text, pattern):
     n = len(text)
     m = len(pattern)
-    
+
+    start_time = time.time()
+
     for i in range(n - m + 1):
         j = 0
         while j < m and text[i+j] == pattern[j]:
             j += 1
         if j == m:
-            return i
+            end_time = time.time()
+            execution_time = end_time - start_time
+            return i, execution_time
     
-    return -1 
+    end_time = time.time()
+    execution_time = end_time - start_time
+    return -1, execution_time
+
+text = "Hello, world!"
+pattern = "world"
+index, time_taken = naive_search(text, pattern)
+print(f"Pattern found at index {index} in {time_taken:.6f} seconds.") 
